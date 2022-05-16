736f496e19a595106e4cdff7fb172afa35c10a0e
JandroVaz <jandrovaz99@gmail.com>
2022-05-16 17:41:24 +0200

    Changes

---
+++ Front/Helper/MailServices.cs
@@ -0,0 +1,54 @@
+﻿using Microsoft.Extensions.Configuration;
+using System.Net;
+using System.Net.Mail;
+
+namespace OjedaGrowShop.Helper
+{
+    public class MailServices
+    {
+
+        private readonly IConfiguration configuration;
+
+        public MailServices(IConfiguration configuration)
+        {
+            this.configuration = configuration; 
+        }
+
+        public void SendEmailOutlook(string receptor, string mensaje = "", string asunto = "Mensaje")
+        {
+            MailMessage mail = new MailMessage();
+
+            string userMail = configuration["usuariooutlook"];
+            string passwordmail = configuration["passwordoutlook"];
+
+            mail.From = new MailAddress(userMail);
+            mail.To.Add(new MailAddress(receptor));
+            mail.Subject = asunto;
+            mail.Body = mensaje;
+            mail.IsBodyHtml = true;
+            mail.Priority = MailPriority.Normal;
+
+            string smtpserver = configuration["host"];
+            int port = int.Parse(configuration["port"]);
+            bool ssl = bool.Parse(configuration["ssl"]);
+            bool defaultcredentials = bool.Parse(configuration["defaultcredentials"]);
+
+            SmtpClient smtpClient = new SmtpClient
+            {
+                Host = smtpserver,
+                Port = port,
+                EnableSsl = ssl,
+                UseDefaultCredentials = defaultcredentials
+            };
+
+            NetworkCredential userCredential = new NetworkCredential(userMail, passwordmail);
+
+            smtpClient.Credentials = userCredential;
+            smtpClient.Send(mail);
+
+            smtpClient.Dispose();
+
+        }
+
+    }
+}
