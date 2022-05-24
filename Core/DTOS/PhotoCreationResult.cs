03dac33fafd1dbe005f3dbf57ce13822172a7554
JandroVaz <jandrovaz99@gmail.com>
2022-05-25 01:02:04 +0200

    New changes

---
+++ Core/DTOS/PhotoCreationResult.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace OjedaGrowShop.EF.DTOS
+{
+    public partial class PhotoCreationResult
+    {
+        public string Name { get; set; }
+        public int OperationResult { get; set; }
+    }
+}
---
+++ Core/DTOS/PhotoStreamWrapper.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+
+namespace OjedaGrowShop.EF.DTOS
+{
+    public partial class PhotoStreamWrapper
+    {
+        public Stream Stream { get; set; }
+        public string Type { get; set; }
+        public string Name { get; set; }
+    }
+}
