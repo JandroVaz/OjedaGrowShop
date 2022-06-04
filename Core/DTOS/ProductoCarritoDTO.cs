9d5134cc0f2b6bc56527d365ef4f669e0b20d1a7
JandroVaz <jandrovaz99@gmail.com>
2022-06-04 17:04:13 +0200

    Total product and delete

---
+++ Core/DTOS/ProductoCarritoDTO.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace OjedaGrowShop.EF.DTOS
+{
+    public partial class ProductoCarritoDTO
+    {
+        public int IdProducto { get; set; }
+        public string  Nombre { get; set; }
+        public double PrecioTotal { get; set; }
+        public double Precio { get; set; }
+        public string Categoria { get; set; }
+        public int Cantidad { get; set; }
+    }
+}
