9e15fc2f2ab5cccd55f123de479f9bd145aa8292
Alejandro Vázquez <58471912+JandroVaz@users.noreply.github.com>
2022-06-24 22:39:52 +0200

    Update OJEDAContext.cs

--- Core/Models/OJEDAContext.cs
+++ Core/Models/OJEDAContext.cs
@@ -34,7 +34,7 @@ namespace OjedaGrowShop.EF.Models
             if (!optionsBuilder.IsConfigured)
             {
 #warning To protect potentially sensitive information in your connection string, you should move it out of source code. You can avoid scaffolding the connection string by using the Name= syntax to read it from configuration - see https://go.microsoft.com/fwlink/?linkid=2131148. For more guidance on storing connection strings, see http://go.microsoft.com/fwlink/?LinkId=723263.
-                optionsBuilder.UseNpgsql("Host=netindio.synology.me;Port=5400;Username=admin;Password=3444;Database=ojeda_db");
+                optionsBuilder.UseNpgsql("Host=______;Port=____;Username=____;Password=____;Database=_____");
             }
         }
 
